d8b8ae60a8a386b7a09b989e953a3fc7368374be
Tyler Grissom <6733216+tsgrissom@users.noreply.github.com>
2024-11-17 18:20:00 -0600

    Started isSteamRunning

--- src/utilities.js
+++ src/utilities.js
@@ -1,4 +1,4 @@
-export function getCountString(count, singularNoun, pluralNoun = null) {
+function getCountString(count, singularNoun, pluralNoun = null) {
     if (typeof(count) !== 'number') {
         console.error('Cannot pluralize count which is not a number');
         return 'ERROR';
@@ -9,3 +9,24 @@ export function getCountString(count, singularNoun, pluralNoun = null) {
     const verbiage = (count === 0 || count > 1) ? pluralNoun : singularNoun;
     return `${count} ${verbiage}`;
 };
+
+function isSteamRunning() {
+    switch (process.platform) {
+        case 'win32':
+            // TODO: Write win32 impl
+            return false;
+        case 'linux':
+            // TODO: Write linux impl
+            console.warn('Platform is not yet supported for function isSteamRunning: Linux');
+            return false;
+        case 'darwin':
+            // TODO: Write macOS impl
+            console.warn('Platform is not yet supported for function isSteamRunning: macOS');
+            return false;
+        default:
+            console.error(`Unable to determine if Steam is running on unknown process platform: ${process.platform}`);
+            return false;
+    }
+}
+
+export { getCountString, isSteamRunning };
